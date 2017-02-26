3c61ea4aca0379781130b8ae8ce23efac1cb8b12
Andréas Livet <andreas.livet@stormshield.eu>
2017-02-26 10:52:55 +0100

    Rng now use OneRNG numbers

--- index.js
+++ index.js
@@ -1,34 +1,54 @@
 "use strict";
 
+const fs = require('fs');
 const WebSocket = require('ws');
 
 const wss = new WebSocket.Server({ port: 8080 });
+const ONERNG_BITRATE = 58000; // In byte per second
 
-console.log('server started at localhost:8080');
+console.log('Server started at localhost:8080');
+
+let rngFd = 0;
+
+// On start up, try to read from oneRng device
+fs.open('/dev/ttyACM0', 'r', function(status, fd) {
+	if (status) {
+		console.error(status.message);
+		if(status.code === 'EACCES') {
+			console.error('You should be root to access OneRNG stream.');
+			process.exit(1);
+		}
+		return;
+	}
+	console.log('Connected to the random number generator');
+	rngFd = fd;
+});
 
 wss.on('connection', function connection(ws) {
 	function randomIntInc (low, high) {
 		return Math.floor(Math.random() * (high - low + 1) + low);
 	}
 
-	console.log('Connection ! ');
+	// TODO : refuse more than one connection
+	console.log('Client connection start sending numbers');
 	function sendBytes () {
-		const bytes = new Uint8Array(25);
-
-		for (var i = 0; i < bytes.length; ++i) {
-			bytes[i] = randomIntInc(0, 255);
-		}
-
-		ws.send(bytes, function (err) {
+		const bytesToRead = ONERNG_BITRATE / 10;
+		const buffer = new Buffer(bytesToRead);
+		fs.read(rngFd, buffer, 0, bytesToRead, null, function(err, num, buff) {
 			if(err) {
-				console.log(err);
+				console.error(err, typeof err, num);
 			}
+			ws.send(buff, function (err) {
+				if(err) {
+					console.error('Error', err);
+				}
+			});
 		});
 	}
 	const id = setInterval(sendBytes, 100);
 
 	ws.on('close', function () {
-		console.log('stopping client interval');
+		console.log('Connection closed. Stopping client interval');
 		clearInterval(id);
 	});
 });
