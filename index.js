f76dafb6840c4b65fe1006da35ad1006d0be8558
Andréas Livet <andreas.livet@gmail.com>
2017-09-25 11:59:30 +0200

    Change the way results are sended
    Do not send numbers while generating them, instead collect them and
    send them at the end of the experiment.
    This way, I assume there is less charge on the RNG during XP (no need to send WS data)
     and clients with low bandwidth can participate.

--- index.js
+++ index.js
@@ -8,9 +8,13 @@ const readUntilLengthReach = require('./read-until');
 
 const ONERNG_CHUNK = 4000; // In byte, so 32000 bits per trials
 const ONERNG_PATH = '/dev/ttyACM0';
+const XP_TRIALS_COUNT = 100;
 
 let rngFd = 0;
 let wsConnection = null;
+// We store all the XP data into a buffer array and then send them at the end
+let xpData = [];
+let xpStarted = false;
 
 const wss = new WebSocket.Server({
 	port: 8080,
@@ -20,17 +24,32 @@ const wss = new WebSocket.Server({
 
 console.log('Server started at localhost:8080');
 
+// Recursively send all xp data buffer
+function sendAllXpData() {
+	const buf = xpData.shift();
+	if(buf) {
+		wsConnection.send(buf, function (err) {
+			if (err) {
+				console.error('Error', err);
+			}
+			sendAllXpData();
+		});
+	}
+}
+
 function readAndSendBytes() {
 	const bytesToRead = ONERNG_CHUNK;
 
 	try {
 		readUntilLengthReach(rngFd, bytesToRead).then(buffer => {
-			if (wsConnection !== null && wsConnection.readyState === WebSocket.OPEN) {
-				wsConnection.send(buffer, function (err) {
-					if (err) {
-						console.error('Error', err);
-					}
-				});
+			if (wsConnection !== null && wsConnection.readyState === WebSocket.OPEN
+				&& xpStarted) {
+				xpData.push(buffer);
+				if(xpData.length === XP_TRIALS_COUNT) {
+					console.log('Xp finished, start sending numbers.');
+					sendAllXpData();
+					xpStarted = false;
+				}
 			}
 			readAndSendBytes();
 		});
@@ -55,11 +74,21 @@ fs.open(ONERNG_PATH, 'r', function (status, fd) {
 });
 
 wss.on('connection', function connection(ws) {
-	console.log('Client connection start sending numbers');
+	console.log('Client connection.');
 	wsConnection = ws;
+	xpStarted = false;
+
+	// User started the experiment
+	ws.on('message', function (msg) {
+		if(msg === 'start') {
+			console.log('Client start XP. Start collecting numbers.')
+			xpStarted = true;
+			xpData = [];
+		}
+	});
 
 	ws.on('close', function () {
-		console.log('Connection closed. Stopping client interval');
+		console.log('Connection closed.');
 		wsConnection = null;
 	});
 });
